d2b994a9e5d770103fdf4ccd287686c3999fafef
dhipgraby <kenneth.zambrano4@gmail.com>
2020-06-20 18:16:54 +0200

    Cat factory Ready

--- client/assets/js/catFactory.js
+++ client/assets/js/catFactory.js
@@ -1,148 +1,185 @@
 
-function catType1() {
-    var firstColor = getColor()
-    var secondColor = getColor()
-    var thirdColor = getColor()
-    var forthColor = getColor()
-    setColors(firstColor, secondColor, thirdColor, forthColor)
-
-}
-
-function catType2() {
-    var firstColor = getColor()
-    var secondColor = getColor()
-    var thirdColor = getColor()
-    var forthColor = getColor()
-    setColors(firstColor, secondColor, thirdColor, forthColor)
-    //Decoration
-    decorationColor(secondColor, thirdColor, forthColor)
-}
-
-function catType3() {
-    var firstColor = getColor()
-    var secondColor = getColor()
-    var thirdColor = getColor()
-    var forthColor = getColor()
-    setColors(firstColor, secondColor, thirdColor, forthColor)
-    //Decoration
-    decorationColor2(secondColor, thirdColor, forthColor)
-}
-function catType4() {
-    var firstColor = getColor()
-    var secondColor = getColor()
-    var thirdColor = getColor()
-    var forthColor = getColor()
-    setColors(firstColor, secondColor, thirdColor, forthColor)
-    //Decoration
-    decorationColor3(secondColor, thirdColor, forthColor)
-}
-
 //Random color
 function getColor() {
     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
     return randomColor
 }
 
-// *** Colors *** //
-
-function setColors(firstColor, secondColor, thirdColor, forthColor) {
-    //Head and chest
-    headColor(firstColor)
-    //Mouth belly
-    mouthAndBelly(secondColor)
-    //Eyes
-    eyeColor(thirdColor)
-    //Eyes shape
-    eyeVariation()
-    //Ears paws
-    earsAndPaw(forthColor)
+function genColors(){
+    var colors = []
+    for(var i = 10; i < 99; i ++){
+    var color = getColor()
+    colors[i] = color
+    }
+    return colors
 }
 
-function headColor(color) {
+function headColor(color,code) {
     $('.cat__head, .cat__chest').css('background', '#' + color)
+    $('#headcode').html('code: '+code)
+    $('#dnabody').html(code)
 }
 
-function mouthAndBelly(color) {
+function mouthAndBelly(color,code) {
     $('.cat__mouth-contour, .cat__tail, .cat__chest_inner').css('background', '#' + color)
+    $('#mouthcode').html('code: '+code)
+    $('#dnamouth').html(code)
 }
 
-function eyeColor(color) {
+function eyeColor(color,code) {
     $('.cat__eye').find('span').css('background', '#' + color)
+    $('#eyecode').html('code: '+code)
+    $('#dnaeyes').html(code)
 }
 
-function earsAndPaw(color) {
+function earsAndPaw(color,code) {
     $('.cat__ear--left, .cat__ear--right, .cat__paw-left, .cat__paw-right,  .cat__paw-left_inner, .cat__paw-right_inner').css('background', '#' + color)
+    $('#earscode').html('code: '+code)
+    $('#dnaears').html(code)
 }
 
-//3 dots single color
-function decorationColor(color) {
+//Middle decoration color
+function midColor(color,code) {
     $('.cat__head-dots').css('background', '#' + color)
+    $('#midcode').html('code: '+code)
+    $('#dnadecorationMid').html(code)
+}
+
+//Sides decoration color
+function SidesColor(color,code) {
     $('.cat__head-dots_first').css('background', '#' + color)
     $('.cat__head-dots_second').css('background', '#' + color)
+    $('#sidecode').html('code: '+code)
+    $('#dnadecorationSides').html(code)
 }
 
-//Two colors
-function decorationColor2(color, secondColor) {
-    $('.cat__head-dots').css('background', '#' + color)
-    $('.cat__head-dots_first').css('background', '#' + secondColor)
-    $('.cat__head-dots_second').css('background', '#' + secondColor)
-}
-
-//3 colors
-function decorationColor3(color, secondColor, thirdColor) {
-    $('.cat__head-dots').css('background', '#' + color)
-    $('.cat__head-dots_first').css('background', '#' + secondColor)
-    $('.cat__head-dots_second').css('background', '#' + thirdColor)
-}
+// Variation functions for range-bars
 
 //8 eye types
 function eyeVariation(num) {
 
-    if (!num) {
-        var num = Math.floor(Math.random() * 7);
-    }
+    $('#dnashape').html(num)
     switch (num) {
-        case 0:
-            normalEyes()
-            break
         case 1:
-            normalEyes().then(() => {
-                return eyesType1()
-            })
+            normalEyes()
+            $('#eyeName').html('Basic')            
             break
         case 2:
             normalEyes().then(() => {
-                return eyesType2()
+                $('#eyeName').html('Chill')
+                return eyesType1()
             })
             break
         case 3:
             normalEyes().then(() => {
-                return eyesType3()
+                $('#eyeName').html('Cute')
+                return eyesType2()
             })
             break
         case 4:
             normalEyes().then(() => {
-                return eyesType4()
+                $('#eyeName').html('Watching')
+                return eyesType3()
             })
             break
         case 5:
             normalEyes().then(() => {
-                return eyesType5()
+                $('#eyeName').html('Night eyes')
+                return eyesType4()
             })
             break
         case 6:
             normalEyes().then(() => {
-                return eyesType6()
+                $('#eyeName').html('Wonder down')
+                return eyesType5()
             })
             break
         case 7:
             normalEyes().then(() => {
+                $('#eyeName').html('Wonder up')
+                return eyesType6()
+            })
+            break
+        case 8:
+            normalEyes().then(() => {
+                $('#eyeName').html('Circle')
                 return eyesType7()
             })
             break
     }
+}
 
 
+//8 decorations types
+function decorationVariation(num) {
+    $('#dnadecoration').html(num)
+    switch (num) {
+        case 1:
+            $('#decorationName').html('Basic')
+            normaldecoration()
+            break
+        case 2:
+            $('#decorationName').html('Inverted')
+            decorationType1()
+            break
+        case 3:
+            $('#decorationName').html('Twisted')
+            decorationType2()
+            break
+        case 4:
+            $('#decorationName').html('Uniform')
+            decorationType3()
+            break
+        case 5:
+            $('#decorationName').html('Uniform twist')
+            decorationType4()
+            break
+        case 6:
+            $('#decorationName').html('Tribal')
+            decorationType5()
+            break
+        case 7:
+            $('#decorationName').html('Propeller')
+            decorationType6()
+            break
+        case 8:
+            $('#decorationName').html('Single')
+            decorationType7()
+            break
+    }
+}
+
+//6 Animations 
+function animationVariation(num) {
+    var specialnum = Math.floor(Math.random() * 10) 
+    $('#dnadanimation').html(num + specialnum)
+
+    switch (num) {
+        case 1:
+            $('#animationName').html('Head')
+            movingHead()
+            break
+        case 2:
+            $('#animationName').html('Tail')
+            movingTail()
+            break
+        case 3:
+            $('#animationName').html('Ears')
+            movingEars()
+            break
+        case 4:
+            $('#animationName').html('Left Ear')
+            leftEar()
+            break
+        case 5:
+            $('#animationName').html('Right Ear')
+            rightEar()
+            break
+        case 6:
+            $('#animationName').html('Attentive')
+            attentiveCat()
+            break
+    }
 }
 
 // **   Eyes **  //
@@ -188,49 +225,139 @@ function eyesType7() {
 
 // ** Angles ** //
 
-function normaldecoration() {
-    var dots = $('.cat__head-dots')    
-    var dots2 = $('.cat__head-dots_first')    
-    var dots3 = $('.cat__head-dots_second')    
-    dots.attr('style','')
-    dots2.attr('style','')
-    dots3.attr('style','')
-}
-
-function noDecoration(){
-    var dots = $('.cat__head-dots')    
-    var dots2 = $('.cat__head-dots_first')    
-    var dots3 = $('.cat__head-dots_second')    
-    dots.removeClass('car__head-dots')
-    dots2.removeClass('car__head-dots_first')
-    dots3.removeClass('car__head-dots_second')
+async function normaldecoration() {
+    //Remove all style from other decorations
+    //In this way we can also use normalDecoration() to reset the decoration style
+    $('.cat__head-dots').css({ "transform": "rotate(0deg)", "height": "48px", "width": "14px", "top": "1px", "border-radius": "0 0 50% 50%" })
+    $('.cat__head-dots_first').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "50% 0 50% 50%" })
+    $('.cat__head-dots_second').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "0 50% 50% 50%" })
 }
 
 //inverted
-function decoration1() {
-    $('.cat__head-dots').css("transform", "rotate(180deg)")
-    $('.cat__head-dots_first').css("transform", "rotate(0deg)")
-    $('.cat__head-dots_second').css("transform", "rotate(0deg)")
+function decorationType1() {
+    normaldecoration().then(() => {
+        $('.cat__head-dots').css("transform", "rotate(180deg)")
+        $('.cat__head-dots_first').css("transform", "rotate(0deg)")
+        $('.cat__head-dots_second').css("transform", "rotate(0deg)")
+    })
+
 }
 
 //Twiss
-function decoration2() {
-    $('.cat__head-dots').css("transform", "rotate(180deg)")
-    $('.cat__head-dots_first').css("transform", "rotate(180deg)")
-    $('.cat__head-dots_second').css("transform", "rotate(180deg)")
+function decorationType2() {
+    normaldecoration().then(() => {
+        $('.cat__head-dots').css("transform", "rotate(180deg)")
+        $('.cat__head-dots_first').css("transform", "rotate(180deg)")
+        $('.cat__head-dots_second').css("transform", "rotate(180deg)")
+    })
+
 }
 
 // ** Parterns **//
 // Unifrom partern
-function decoration3() {
-    normaldecoration()
-    $('.cat__head-dots, .cat__head-dots_first, .cat__head-dots_second').css({ "height": "40px","width": "20px" })    
+function decorationType3() {
+    normaldecoration().then(() => {
+        $('.cat__head-dots, .cat__head-dots_first, .cat__head-dots_second').css({ "height": "40px", "width": "20px" })
+    })
+
+}
+
+//Combination of 3 and 4
+function decorationType4() {
+    normaldecoration().then(() => {
+        $('.cat__head-dots, .cat__head-dots_first, .cat__head-dots_second').css({ "height": "40px", "width": "20px", "transform": "rotate(180deg)" })
+    })
+
+}
+
+//Tribal decoration
+function decorationType5() {
+    normaldecoration().then(() => {
+        $('.cat__head-dots').css({ "transform": "rotate(180deg)", "height": "50px", "border-radius": "50% 50% 50% 50%" })
+        $('.cat__head-dots_first').css({ "transform": "rotate(35deg)", "height": "40px" })
+        $('.cat__head-dots_second').css({ "transform": "rotate(-35deg)", "height": "40px" })
+    })
+
+}
+
+//Sides down
+function decorationType6() {
+    normaldecoration().then(() => {
+        $('.cat__head-dots').css({ "transform": "rotate(180deg)", "height": "50px", "border-radius": "50% 50% 50% 50%" })
+        $('.cat__head-dots_first').css({ "transform": "rotate(135deg)", "height": "45px", "top": "-25px" })
+        $('.cat__head-dots_second').css({ "transform": "rotate(-135deg)", "height": "45px", "top": "-25px" })
+    })
+
+}
+
+//Single dot decoration
+function decorationType7() {
+    normaldecoration().then(() => {
+        var dots2 = $('.cat__head-dots_first')
+        var dots3 = $('.cat__head-dots_second')
+        dots2.css('height', '0px')
+        dots3.css('height', '0px')
+    })
+
+}
+
+/** Animations **/
+
+async function resetAnimation() {
+    document.getElementById("head").classList.remove("movingHead")
+    document.getElementById("leftEar").classList.remove("movingEarsLeft", "moving-Single-EarLeft", "attentionLeft")
+    document.getElementById("rightEar").classList.remove("movingEarsRight", "moving-Single-EarRight", "attentionRight")
+    document.getElementById("tail").classList.remove("movingTail")
+
+}
+
+function movingHead() {
+    resetAnimation().then(() => {
+        $('#head').addClass('movingHead')
+        $('#leftEar').addClass('movingEarsLeft')
+        $('#rightEar').addClass('movingEarsRight')
+    })
+}
+
+function movingTail() {
+    resetAnimation().then(() => {
+        $('#tail').addClass('movingTail')
+    })
+}
+
+//moving both ears
+function movingEars() {
+    resetAnimation().then(() => {
+        $('#leftEar').addClass('movingEarsLeft')
+        $('#rightEar').addClass('movingEarsRight')
+    })
+}
+
+// Single Ears
+
+function leftEar() {
+    resetAnimation().then(() => {
+        $('#leftEar').addClass('moving-Single-EarLeft')
+    })
+}
+
+function rightEar() {
+    resetAnimation().then(() => {
+        $('#rightEar').addClass('moving-Single-EarRight')
+    })
+}
+
+// Attentive Cat Ears animation
+
+function attentiveCat() {
+    resetAnimation().then(() => {
+        $('#leftEar').addClass('attentionLeft')
+        $('#rightEar').addClass('attentionRight')
+    })
 }
 
 
-
-
-
+// Eyes of the car followign the cursor
 const closer = 4;
 const further = -4;
 
--- client/index.js
+++ client/index.js
@@ -31,159 +31,35 @@ $(document).ready(function() {
     /*
     *   When  you value are generated send me a uint32
     */
+    var dnaStr = getDna()
 
-    //var dnaStr = $("#dna").val()
-
-    // console.log(instance);
+    console.log(instance);
 
   //  KittyCore.createPromoKitty(dna, accounts(0));
-    // instance.methods.createKitty(0, 0, 0, dnaStr, user).send({from: user, gas: 100000});
+    instance.methods.createKitty(0, 0, 0, dnaStr, user).send({from: user, gas: 100000});
   })
 
   $("#generateKitty").click(generateKitty);
 
 });
 
-
-
 // take the Kitty dna, and update our image
 function generateKitty() {
 
-  dnaStr = String(Math.floor(Math.random()*1E16))
-  console.log("dnaStr");
-  console.log(dnaStr);
+  dnaStr = String(Math.floor(Math.random()*1E16))  
 
-  let kittyDetails = {
-    
-    // first 2 digits make up the head. We have 7 possible heads, so % 7
-    // to get a number 0 - 6, then add 1 to make it 1 - 7. Then we have 7
-    // image files named "head1.png" through "head7.png" we load based on
-    // this number:
-    bodyChoice: dnaStr.substring(0, 2) % 10 + 1,
-    
-    // 2nd 2 digits make up the eyes, 11 variations:
-    eyeChoice: dnaStr.substring(2, 4) % 11 + 1,
-    
-    // 3 variations of tail:
-    tailChoice: dnaStr.substring(4, 6) % 3 + 1,
-
-    // 6 variations of tongue:
-    tongueChoice: dnaStr.substring(6, 8) % 6 + 1,
-    
-    // TODO ADD AN OTHER PART OF THE BODY:
-    Choice: dnaStr.substring(8, 10) % 6 + 1,
-
-    // last 6 digits control color. Updated using CSS filter: hue-rotate
-    // which has 360 degrees:
-    ColorChoice: parseInt(dnaStr.substring(10, 12) / 100 * 360),
-    
-    eyeColorChoice: parseInt(dnaStr.substring(12, 14) / 100 * 360),
-    
-    tailColorChoice: parseInt(dnaStr.substring(14, 16) / 100 * 360),
-    
+  let kittyDetails = {    
     ownerAddr: user,
     KittyDescription: "This is my first kitty",
   }
   generateCatCss(kittyDetails);
 }
 
-function generateCatCss(kittyDetails){
-
-  console.log(kittyDetails);
-
-// Set the kitty body Shape
-  // default height 130 / width 110
-  // default ear-right 6 / width 110
-
-  let height = 130 + kittyDetails.bodyChoice * 4;
-  let width = 110 + kittyDetails.bodyChoice;
-  let earRight = 5 + kittyDetails.bodyChoice;
-
-  $("#body").css('height', height);
-  $("#body").css('width', width);
-  $("#ear-right").css('margin-left', earRight);
-
-
-// Set the kitty eyes
-
-  //default height/width: 18px;
-  //default border-radius: 50%;
-  let eyeValue = 0;
-  if (kittyDetails.eyeChoice > 5){
-    eyeValue += kittyDetails.eyeChoice;
-  } else {
-    eyeValue -= kittyDetails.eyeChoice;
-  }
-
-  let eyesShape = 50 - kittyDetails.eyeChoice*2;
-  let eyesSizePos = 30 - kittyDetails.eyeChoice;
-  let eyesSize = eyeValue + 18;
-
-  $(".eye").css('height', eyesSize);
-  $(".eye").css('width', eyesSize);
-  $(".eye").css('border-radius', eyesShape + "%");
-  $("#eye").css('margin-top', eyesSizePos + "%");
-
-// Special condition for big eyes and small body
-  if (kittyDetails.bodyChoice <= 4 && kittyDetails.eyeChoice >= 6){
-    $("#tummy").css('margin-top', "5%");
-  } else {
-    $("#tummy").css('margin-top', "20%");
-  }
-
-//  Set the kitty color
-// First option
-   var mycolor =  (parseInt("E9CBA7", 16) + kittyDetails.ColorChoice).toString(16) 
-  $("#body").css('background-color', "#" + mycolor + "FF");
-  $("#mask").css('background-color', "#" + mycolor + "FF");
-  $(".ear").css('background-color', "#" + mycolor + "FF");
-
-// Check with or without the filter but i think the filter is really cool
-  $("#body").css('filter', "hue-rotate(" + kittyDetails.ColorChoice + "deg)");
-
-// Set the kitty tail
-
-  // Border 15px
-  if (kittyDetails.tailChoice === 1){
-    // $("#tail-end").css('border-radius', "1%");
-    // $("#tail-end").css('margin-left', "125px");
-    // $("#tail-end").css('height', "13px");
-    // $("#tail-end").css('width', "13px");
-    $("#tail").css('border-top-width', "10px");
-    $("#tail").css('border-left-width', "10px");
-    $("#tail").css('border-right-width', "10px");
-  } else if (kittyDetails.tailChoice === 2){
-    // $("#tail-end").css('border-radius', "50%");
-    // $("#tail-end").css('margin-left', "130px");
-    // $("#tail-end").css('height', "17px");
-    // $("#tail-end").css('width', "17px");
-    $("#tail").css('border-top-width', "15px");
-    $("#tail").css('border-left-width', "15px");
-    $("#tail").css('border-right-width', "15px");
-  } else if (kittyDetails.tailChoice === 3){
-
-    // Tail third option TODO
-
-    // $("#tail-end").css('border-radius', "1%");
-    // $("#tail-end").css('margin-left', "125px");
-    // $("#tail-end").css('height', "13px");
-    // $("#tail-end").css('width', "13px");
-    $("#tail").css('border-top-width', "10px");
-    $("#tail").css('border-left-width', "10px");
-    $("#tail").css('border-right-width', "10px");
-  }
-// Set the kitty tongue
-  // TODO
-}
-
 function displayKittyInfo(owner, kittyId, mumId, dadId, genes) {
+  $("kittytable").removeClass('hidden')
   $("#kittyOnwer").text(owner);
   $("#kittyId").text(kittyId);
   $("#kittyMum").text(mumId);
   $("#kittyDad").text(dadId);
   $("#kittyGenes").text(genes);
-}
-
-function log(str){
-  return  console.log(str)
 }
